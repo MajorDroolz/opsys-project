27afc349c25f8fe94b003b8902552f1b073f85ae
Lawrence M <lawrencejunzh@gmail.com>
2024-02-26 15:20:27 -0500

    force change fname

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Opsys Project
 
 Yeah, put stuff here.
+
+sample usage:
+
+```python main.py 3 1 1024 0.001 3000```
--- main.py
+++ main.py
@@ -48,4 +48,3 @@ if __name__ == "__main__":
     print(bursts())
     print(bursts())
     print(bursts())
-    print(bursts())
