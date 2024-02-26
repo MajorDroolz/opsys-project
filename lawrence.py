914b03a33dd322c25c8ca711e239fae5ed7671eb
Lawrence M <lawrencejunzh@gmail.com>
2024-02-26 15:15:31 -0500

    organize file

--- lawrence.py
+++ lawrence.py
@@ -3,38 +3,49 @@ import math
 import os
 import sys
 
-if len(sys.argv) != 6:
-    print("Usage: python script.py n n_cpu seed lambda_arg e")
-    sys.exit(1)
-
-_, n, n_cpu, seed, lambda_arg, e = sys.argv
-print(f"<<< PROJECT PART I -- process set (n={n}) with {n_cpu} CPU-bound process >>>")
-# print(f"I/O-bound process {}: arrival time {}ms; {} CPU bursts:")
-# print(f"--> CPU burst {}ms --> I/O burst {}ms")
-
-random_gen = Rand48(0)
-
 
 def next_exp():
-    return 30.5
+    return random.next()
 
 
+# 1
 def arrival():
     return math.floor(next_exp())
 
 
+# 2
 def bursts():
-    return math.ceil(random_gen.next() * 64)
+    # this doesnt work yet
+    return math.ceil(random.srand(1) * 64)
 
 
-print(bursts())
-print(bursts())
-print(bursts())
-print(bursts())
-print(bursts())
-print(bursts())
-print(bursts())
-print(bursts())
-print(bursts())
-print(bursts())
-print(bursts())
+# 3
+
+
+if __name__ == "__main__":
+    # error check arguments
+    if len(sys.argv) != 6:
+        print("Usage: python script.py n n_cpu seed lambda_arg e")
+        sys.exit(1)
+
+    _, n, n_cpu, seed, lambda_arg, e = sys.argv
+    print(
+        f"<<< PROJECT PART I -- process set (n={n}) with {n_cpu} CPU-bound process >>>"
+    )
+    # print(f"I/O-bound process {}: arrival time {}ms; {} CPU bursts:")
+    # print(f"--> CPU burst {}ms --> I/O burst {}ms")
+
+    # This is NOT the python library module.
+    random = Rand48(0)
+
+    print(bursts())
+    print(bursts())
+    print(bursts())
+    print(bursts())
+    print(bursts())
+    print(bursts())
+    print(bursts())
+    print(bursts())
+    print(bursts())
+    print(bursts())
+    print(bursts())
