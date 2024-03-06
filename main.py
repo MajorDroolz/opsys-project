deed1c031137e024ab6d38cae66c19864edb04ab
Lawrence M <lawrencejunzh@gmail.com>
2024-03-06 00:53:34 -0500

    fix next_exp()

--- main.py
+++ main.py
@@ -4,23 +4,18 @@ import os
 import sys
 
 
-def next_exp():
-    return random.next()
-
-
 # 1
-def arrival():
-    return math.floor(next_exp())
+def arrival(random):
+    return math.floor(random.next_exp())
 
 
 # 2
-def bursts():
-    # this doesnt work yet
-    return math.ceil(random.srand(1) * 64)
+def bursts(random):
+    return math.ceil(random.drand()) * 64
 
 
 # 3
-def generate_process(n):
+def generate_process(n, random):
     processes = []
     for _ in range(n):
         process = {}
@@ -28,8 +23,8 @@ def generate_process(n):
         process["cpu_bursts"] = bursts()
         process["bursts"] = []
         for i in range(process["cpu_bursts"]):
-            cpu_burst_time = math.ceil(next_exp())
-            io_burst_time = math.ceil(next_exp()) * 10
+            cpu_burst_time = math.ceil(random.next_exp())
+            io_burst_time = math.ceil(random.next_exp()) * 10
             if i >= n - 26:
                 cpu_burst_time *= 4
                 io_burst_time = math.ceil(io_burst_time / 8)
@@ -47,22 +42,23 @@ if __name__ == "__main__":
         print("Usage: python script.py n n_cpu seed lambda_arg threshold")
         sys.exit(1)
 
-    _, n, n_cpu, seed, lambda_arg, threshold = sys.argv
+    n = int(sys.argv[1])
+    n_cpu = int(sys.argv[2])
+    seed = int(sys.argv[3])
+    lambda_arg = float(sys.argv[4])
+    threshold = int(sys.argv[5])
     print(
         f"<<< PROJECT PART I -- process set (n={n}) with {n_cpu} CPU-bound process >>>"
     )
+
     # print(f"I/O-bound process {}: arrival time {}ms; {} CPU bursts:")
     # print(f"--> CPU burst {}ms --> I/O burst {}ms")
 
     # This is NOT the python library module.
-    random = Rand48(0)
+    random = Rand48(seed, lambda_arg, threshold)
 
-    print(bursts())
-    print(bursts())
-    print(bursts())
-    print(bursts())
-    print(bursts())
-    print(bursts())
-    print(bursts())
-    print(bursts())
-    print(bursts())
+    print(arrival(random))
+    print(arrival(random))
+    print(arrival(random))
+    print(arrival(random))
+    print(arrival(random))
--- randomc/random.py
+++ randomc/random.py
@@ -1,9 +1,14 @@
+import math
+
+
 # Exact algorithm suite in `glibc`.
 # Credit to Dietrich Epp on Stack Overflow.
 # https://stackoverflow.com/a/7287046
 class Rand48(object):
-    def __init__(self, seed):
+    def __init__(self, seed, lambda_arg=0.001, threshold=3000):
         self.n = seed
+        self.lambda_arg = lambda_arg
+        self.threshold = threshold
 
     def seed(self, seed):
         self.n = seed
@@ -26,6 +31,14 @@ class Rand48(object):
         if n & (1 << 31):
             n -= 1 << 32
         return n
+
+    # custom implementation of next_exp
+    def next_exp(self):
+        value = -math.log(self.drand()) / self.lambda_arg
+        if value > self.threshold:
+            return self.next_exp()
+        else:
+            return value
 
 
 if __name__ == "__main__":
